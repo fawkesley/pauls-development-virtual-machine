dd4e2e0b519c896c25b3799044a80e24dd964667
Paul M Furley <paul@paulfurley.com>
2016-02-25 17:02:40 +0000

    Initial commit

---
+++ config/provision.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -eux
+
+install_extra_ppas() {
+    # add heroku repository to apt
+    echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
+
+    # install heroku's release key for package verification
+    wget -q -O- https://toolbelt.heroku.com/apt/release.key | apt-key add -
+}
+
+update_package_index() {
+    sudo apt-get update
+}
+
+install_required_packages() {
+    sudo apt-get install -y \
+	git \
+	tree \
+	zip \
+	unzip \
+	htop \
+	whois \
+	ack-grep \
+	sqlite3 \
+	run-one \
+	libpq-dev \
+	python-dev \
+	python3-dev \
+	libxml2-dev \
+	libxslt1-dev \
+	libfreetype6-dev \
+	libimage-exiftool-perl \
+	postgresql \
+	heroku-toolbelt \
+	xclip \
+	markdown \
+	nodejs-legacy \
+	npm \
+	gnupg-curl \
+	python-flake8 \
+	python-virtualenv \
+	virtualenvwrapper
+}
+
+configure_ack() {
+    sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
+}
+
+install_bashrc_symlink() {
+    ln -sf /vagrant/config/bashrc /home/vagrant/.bashrc
+}
+
+
+install_bashrc_symlink
+install_extra_ppas
+update_package_index
+install_required_packages
+configure_ack
